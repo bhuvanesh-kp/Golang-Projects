8f6ee8ebfbec124721d91b273de1c832054fffa4
Arques <bhuvaneshkpb@gmail.com>
2026-03-19 17:26:34 +0530

    [bhuvanesh][modification]:implementing sleeping barber

---
+++ sleeping-barber/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main(){
+	// seed random number generator
+
+	// print starting message 
+
+	// create channels if we need any
+
+	// create the barbershop
+
+	// add barbers
+
+	// start the barbershop as a go routine
+
+	// add clients
+
+	// block untill the barbershop is closed
+}
