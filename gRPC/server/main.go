9177a1921c5a34811cbba03f47f57ec654e57b88
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2026-04-12 00:13:43 +0530

    feat: gRPC server added

--- gRPC/server/main.go
+++ gRPC/server/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"context"
+	pb "grpc_pingpong/pingpong"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	ping pb.UnimplementedPingPongServiceServer
+}
+
+func (s *server) SendPing(ctx context.Context, req *pb.PingRequest) (*pb.PongResponse, error) {
+	log.Println("Received message: ", req.GetMessage())
+	return &pb.PongResponse{Message: "Pong!"}, nil
+}
+
 func main() {
-	
+	lis, err := net.Listen("tcp", ":5001")
+	if err != nil {
+		log.Println("failed to listen: ", err)
+		return
+	}
+
+	s := grpc.NewServer()
+	var NewServer server
+	pb.RegisterPingPongServiceServer(s, &NewServer.ping)
+
+	log.Println("Server listening to port: 5001")
+	if err := s.Serve(lis); err != nil {
+		log.Println("failed to server: ", err)
+		return
+	}
 }
